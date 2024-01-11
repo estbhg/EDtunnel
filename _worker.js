f595fedaf65b9f51f132acfb25aa87729e068748
woxproject <156349788+woxpro@users.noreply.github.com>
2024-01-12 00:06:59 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '47fb0431-0383-4e50-9865-aae363465f0a';
+let userID = 'f5a717f5-2f22-4509-ad6f-b3f623e35bc4';
 
-const proxyIPs = ['168.138.177.109'];
+const proxyIPs = ['51.79.250.140'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
